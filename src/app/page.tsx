59c12407542dc0e642a9ab7db5c9cc76605a1981
wickfirm <mohamed@thewickfirm.com>
2025-12-27 18:39:57 +0400

    Add Home component with session-based redirect

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  redirect(session ? "/dashboard" : "/login");
+}
