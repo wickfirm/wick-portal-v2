f4b55c91b8a912dfc0ecfc2fd6b742100c15b65a
wickfirm <mohamed@thewickfirm.com>
2026-01-20 01:13:01 +0200

    Update providers.tsx

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { QueryProvider } from "@/providers/query-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryProvider>{children}</QueryProvider>
+    </SessionProvider>
+  );
 }
